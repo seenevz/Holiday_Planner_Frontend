91064340304a05a9ab44ed91660d95517b0291d7
seenevz <serneves@hotmail.com>
2020-03-14 20:20:04 +0000

    Changes to home container

--- src/containers/CurrentTripAllPlaces.js
+++ src/containers/CurrentTripAllPlaces.js
@@ -1,9 +1,20 @@
 import React from "react";
 import { connect } from "react-redux";
+import gql from "graphql-tag";
 
+const TRIP_QUERY = gql`
+   query trip($tripId: Strig!) {
+      trip(tripId: $tripId) {
+         title
+         places {
+            id
+         }
+      }
+   }
+`;
 class CurrentTripAllPlaces extends React.Component {
    render() {
-      return <div>Current Trip Name: </div>;
+      return <div>Current Trip Name:{this.props.trip.name} </div>;
    }
 }
 
